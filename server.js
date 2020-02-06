12b9c63922ec016a7b84930e4520e070b002a5b1
Pam Deininger <pameladeininger@gmail.com>
2020-02-05 19:43:33 -0500

    corrections

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 //Dependencies
+//interaction with front end
 const express = require("express");
+//path file names
 const path = require("path");
+//to read and  write files
 const fs = require("fs");
+//express server
 const app = express();
-const db = require("./public/index.html");
+const db = require("./db/db.json");
+//set for listening
 const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(_dirname, './public')))
+
 
 //get API notes-sends user to AJAX page
 app.get("/", function(req, res) {
@@ -38,6 +48,9 @@ app.delete("/api/notes/:id", function(req, res) {
   console.log(chosenNote);
   console.log(db);
 });
+
+db.splice(chosenNote, 1);
+
 
 //server listening
 app.listen(PORT, function() {
